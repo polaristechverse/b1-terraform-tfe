0fb721a1ee0412c268ee0922aeb7544a0fc0713a
chaitanya <chaitanya@gmail.com>
2025-11-29 07:37:05 +0530

    added new subnet cidr

--- network.tf
+++ network.tf
@@ -3,7 +3,7 @@ module "prod_network" {
   version             = "1.0.0"
   vpc_cidr_block      = "172.16.0.0/16"
   vpc_name            = "PolarisProd"
-  Public_Subnet_CIDR  = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
+  Public_Subnet_CIDR  = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24","172.16.4.0/24"]
   Private_Subnet_CIDR = ["172.16.10.0/24", "172.16.20.0/24", "172.16.30.0/24"]
   az                  = ["us-east-1a", "us-east-1b", "us-east-1c"]
   env                 = "Prod"
